feee26c3979eb1c9abe3a9e449d773f8974ac6c3
serhiiyakunin <serhiiyakunin22@gmail.com>
2024-06-12 14:20:37 +0300

    added Form and Productlist components

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import './Form.css'
+import { useTelegram } from '../../hooks/useTelegram';
+
+const Form = () => {
+    const [country, setCountry] = useState('');
+    const [street, setStreet] = useState('');
+    const [subject, setSubject] = useState('physical');
+    const { tg } = useTelegram();
+
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: 'Надіслати данні'
+        })
+    }, [])
+
+    useEffect(() => {
+        if (!street || !country) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }, [country, street])
+
+    const onChangeCountry = (e) => {
+        setCountry(e.target.value)
+    }
+
+    const onChangeStreet = (e) => {
+        setStreet(e.target.value)
+    }
+
+    const onChangeSubject = (e) => {
+        setSubject(e.target.value)
+    }
+
+    return (
+        <div className={"form"}>
+            <h3>Введіть ваші данні</h3>
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Країна'}
+                value={country}
+                onChange={onChangeCountry}
+            />
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Вулиця'}
+                value={street}
+                onChange={onChangeStreet}
+            />
+            <select value={subject} onChange={onChangeSubject} className={'select'}>
+                <option value={'physical'}>Физ. особа</option>
+                <option value={'legal'}>Юр. особа</option>
+            </select>
+        </div>
+    );
+};
+
+
+
+export default Form;
---
+++ src/components/ProductList/ProductList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ProductList.css';
+
+const ProductList = () => {
+    return (
+        <div>
+            PRODUCTLIST
+        </div>
+    );
+};
+
+
+
+export default ProductList;
