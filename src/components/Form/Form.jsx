7b1d263add7e36d80ba015516f73b9460c42c445
serhiiyakunin <serhiiyakunin22@gmail.com>
2024-06-13 10:27:29 +0300

    Corrected onSendData

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -18,11 +18,11 @@ const Form = () => {
     }, [])
 
     useEffect(() => {
-        tg.WebApp.onEvent('mainButtonClicked', onSendData)
+        tg.onEvent('mainButtonClicked', onSendData)
         return () => {
-            tg.WebApp.offEvent('mainButtonClicked', onSendData)
+            tg.offEvent('mainButtonClicked', onSendData)
         }
-    }, [])
+    }, [onSendData])
 
     useEffect(() => {
         tg.MainButton.setParams({
