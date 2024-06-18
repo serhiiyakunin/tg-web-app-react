8672922f7fc378b0e373ff3c24327f176a46eeb6
serhiiyakunin <serhiiyakunin22@gmail.com>
2024-06-18 12:21:57 +0300

    Corrected ProductList

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from '../Button/Button';
+import './ProductItem.css';
 
 const ProductItem = ({ product, className, onAdd }) => {
     const onAddHandler = () => {
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './ProductList.css';
 import ProductItem from '../ProductItem/ProductItem';
 import { useTelegram } from '../../hooks/useTelegram';
+import { useCallback, useEffect, useState } from 'react';
 
 const products = [
     { id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые' },
@@ -20,30 +21,32 @@ const getTotalPrice = (items = []) => {
     }, 0)
 }
 
-const onAdd = (product) => {
-    const alreadyAdded = addedItems.find(item => item.id === product.id);
-    let newItems = [];
 
-    if (alreadyAdded) {
-        newItems = addedItems.filter(item => item.id !== product.id);
-    } else {
-        newItems = [...addedItems, product];
-    }
-    setAddedItems(newItems)
-
-    if (newItems.length === 0) {
-        tg.MainButton.hide();
-    } else {
-        tg.MainButton.show();
-        tg.MainButton.setParams({
-            text: `Купити ${getTotalPrice(newItems)}`
-        })
-    }
-}
 
 const ProductList = () => {
     const [addedItems, setAddedItems] = useState([]);
     const { tg, queryId } = useTelegram();
+
+    const onAdd = (product) => {
+        const alreadyAdded = addedItems.find(item => item.id === product.id);
+        let newItems = [];
+
+        if (alreadyAdded) {
+            newItems = addedItems.filter(item => item.id !== product.id);
+        } else {
+            newItems = [...addedItems, product];
+        }
+        setAddedItems(newItems)
+
+        if (newItems.length === 0) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+            tg.MainButton.setParams({
+                text: `Купити ${getTotalPrice(newItems)}`
+            })
+        }
+    }
 
     return (
         <div className={'list'}>
